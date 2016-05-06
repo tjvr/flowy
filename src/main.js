1588a5beac687ce4447ab9d1fd5e049a4cab2bb6
tjvr <tim@tjvr.org>
2016-05-06 01:06:08 +0100

    Drag and drop!

--- src/main.js
+++ src/main.js
@@ -41,6 +41,14 @@ function el(tagName, className) {
 
 /*****************************************************************************/
 
+function containsPoint(extent, x, y) {
+  return x >= 0 && y >= 0 && x < extent.width && y < extent.height;
+}
+
+function opaqueAt(context, x, y) {
+  return containsPoint(context.canvas, x, y) && context.getImageData(x, y, 1, 1).data[3] > 0;
+}
+
 function bezel(context, path, thisArg, inset, scale) {
   if (scale == null) scale = 1;
   var s = inset ? -1 : 1;
@@ -121,8 +129,8 @@ class Drawable {
   }
 
   moveTo(x, y) {
-    this.x = x;
-    this.y = y;
+    this.x = x | 0;
+    this.y = y | 0;
     this.el.style.transform = `translate(${x}px, ${y}px)`;
   }
 
@@ -161,6 +169,60 @@ class Drawable {
 
   // layoutSelf() {}
   // draw() {}
+
+  get app() {
+    var o = this;
+    while (o && !o.isApp) {
+      o = o.parent;
+    }
+    return o;
+  }
+
+  get workspacePosition() {
+    var o = this;
+    var x = 0;
+    var y = 0;
+    while (o && !o.isWorkspace) {
+      x += o.x;
+      y += o.y;
+      o = o.parent;
+    }
+    return {x: x, y: y};
+  }
+
+  get worldPosition() {
+    var o = this;
+    var x = 0;
+    var y = 0;
+    while (o && !o.isWorkspace) {
+      x += o.x;
+      y += o.y;
+      o = o.parent;
+    }
+    if (o) {
+      x *= 1; // o._scale;
+      y *= 1; // o._scale;
+      var bb = o.el.getBoundingClientRect();
+      x += Math.round(bb.left);
+      y += Math.round(bb.top);
+      if (o.el !== document.body) {
+        // x -= o.scrollX;
+        // y -= o.scrollY;
+      }
+    }
+    return {x: x, y: y};
+  }
+
+  get topScript() {
+    var o = this;
+    while (o.parent) {
+      if (o.parent.isWorkspace) return o;
+      o = o.parent;
+    }
+    return null;
+  }
+
+  click() {}
 }
 
 
@@ -185,6 +247,10 @@ class Label extends Drawable {
   layoutSelf() {}
   drawChildren() {}
   draw() {}
+
+  get dragObject() {
+    return this.parent;
+  }
 }
 Label.measure = createMetrics('label');
 
@@ -198,9 +264,14 @@ class Operator extends Drawable {
     this.el.appendChild(this.canvas);
     this.context = this.canvas.getContext('2d');
 
+    this.parts = [];
+    this.labels = [];
+    this.args = [];
+
     this.info = info;
-    this.parts = parts;
-    parts.forEach(w => this.el.appendChild(w.el));
+    for (var i=0; i<parts.length; i++) {
+      this.add(parts[i]);
+    }
 
     this.color = '#7a48c3';
   }
@@ -209,6 +280,85 @@ class Operator extends Drawable {
   set color(value) {
     this._color = value;
     this.redraw();
+  }
+
+  add(part) {
+    if (part.parent) part.parent.remove(part);
+    part.parent = this;
+    this.parts.push(part);
+    if (this.parent) part.layoutChildren(); // TODO
+    this.layout();
+    this.el.appendChild(part.el);
+
+    var array = part.isOperator ? this.args : this.labels;
+    array.push(part);
+  }
+  
+  replace(oldPart, newPart) {
+    if (oldPart.parent !== this) return;
+    if (newPart.parent) newPart.parent.remove(newPart);
+    oldPart.parent = null;
+    newPart.parent = this;
+
+    var index = this.parts.indexOf(oldPart);
+    this.parts.splice(index, 1, newPart);
+
+    var array = oldPart.isArg || oldPart.isOperator ? this.args : this.labels;
+    var index = array.indexOf(oldPart);
+    array.splice(index, 1, newPart);
+
+    newPart.layoutChildren();
+    this.layout();
+    if (this.workspace) newPart.drawChildren();
+
+    this.el.replaceChild(newPart.el, oldPart.el);
+  };
+
+  remove(part) {
+    if (part.parent !== this) return;
+    part.parent = null;
+    var index = this.parts.indexOf(part);
+    this.parts.splice(index, 1);
+    this.el.removeChild(part.el);
+
+    var array = part.isOperator ? this.args : this.labels;
+    var index = array.indexOf(part);
+    array.splice(index, 1);
+  }
+
+  reset(arg) {
+    if (arg.parent !== this || !arg.isOperator) return this;
+
+    var i = this.args.indexOf(arg);
+    this.replace(arg, new Label("[]"));
+  };
+  
+  detach() {
+    if (this.workspace.isPalette) {
+      return this.scriptCopy();
+    }
+    if (this.parent.isOperator) {
+      this.parent.reset(this);
+      // return this; //new Script().setScale(this._scale).add(this);
+    }
+    return this;
+    // if (this.parent.isScript) {
+    //   return this.parent.splitAt(this);
+    // }
+  }
+
+  objectFromPoint(x, y) {
+    var args = this.args;
+    for (var i = args.length; i--;) {
+      var arg = args[i];
+      var o = arg.objectFromPoint(x - arg.x, y - arg.y);
+      if (o) return o;
+    }
+    return opaqueAt(this.context, x * density, y * density) ? this : null;
+  }
+
+  get dragObject() {
+    return this;
   }
 
   layoutChildren() {
@@ -275,7 +425,7 @@ class Operator extends Drawable {
     context.arc(w - r, r + .5, r, PI32, 0, false);
     context.arc(w - r, h - r - .5, r, 0, PI12, false);
     context.arc(r, h - r - .5, r, PI12, PI, false);
-  };
+  }
 
   draw() {
     this.canvas.width = this.ownWidth * density;
@@ -291,101 +441,7 @@ class Operator extends Drawable {
     bezel(context, this.pathBlock, this, false, density);
   }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+Operator.prototype.isOperator = true; // TODO
 
 
 
@@ -429,7 +485,9 @@ class World {
   constructor() {
     this.camera = new Camera();
     this.factor = 1;
-    this.el = el('world');
+    this.el = this.elContents = el('world no-select');
+
+    this.scripts = [];
 
     window.addEventListener('resize', this.resize.bind(this));
     window.addEventListener('wheel', this.wheel.bind(this));
@@ -447,19 +505,74 @@ class World {
     this.resize();
     this.tick();
 
-    window.x = new Operator({}, [
+    this.fingers = [];
+    window.addEventListener('mousedown', this.mouseDown.bind(this));
+    window.addEventListener('mousemove', this.mouseMove.bind(this));
+    window.addEventListener('mouseup', this.mouseUp.bind(this));
+
+    this.add(new Operator({}, [
       new Label("bob"),
       new Operator({}, [
         new Label("cow"),
       ]),
       new Label("fred"),
-    ]);
-    setTimeout(() => {
-      x.layoutChildren();
-      //l.layoutChildren();
-      //l.drawChildren();
-      this.el.appendChild(x.el);
-    });
+    ]));
+  }
+
+  add(script) {
+    if (script.parent) script.parent.remove(script);
+    script.parent = this;
+    this.scripts.push(script)
+    script.layoutChildren();
+    script.drawChildren();
+    this.elContents.appendChild(script.el);
+  }
+
+  remove() {}
+
+  grab(script, offsetX, offsetY, g) {
+    if (!g) g = this.createGesture(this);
+    this.drop(g);
+    g.dragging = true;
+
+    if (offsetX === undefined) {
+      var pos = script.worldPosition;
+      offsetX = pos.x - g.pressX;
+      offsetY = pos.y - g.pressY;
+    }
+    g.dragX = offsetX;
+    g.dragY = offsetY;
+    assert(''+offsetX !== 'NaN');
+
+    if (script.parent) {
+      script.parent.remove(script);
+    }
+
+    g.dragScript = script;
+    var pos = this.camera.fromScreen(g.dragX + g.mouseX, g.dragY + g.mouseY);
+    g.dragScript.moveTo(pos.x | 0, -pos.y | 0);
+    g.dragScript.parent = this;
+    this.elContents.appendChild(g.dragScript.el);
+    g.dragScript.layoutChildren();
+    g.dragScript.drawChildren();
+    // g.dragScript.addShadow(this.dragShadowX, this.dragShadowY, this.dragShadowBlur, this.dragShadowColor);
+    // this.showFeedback(g);
+  }
+
+  drop(g) {
+    if (!g) g = this.getGesture(this);
+    if (!g.dragging) return;
+
+    // TODO
+
+    g.dragging = false;
+    g.dragPos = null;
+    g.dragState = null;
+    g.dragWorkspace = null;
+    g.dragScript = null;
+    g.dropWorkspace = null;
+    g.feedbackInfo = null;
+    g.commandScript = null;
   }
 
   tick() {
@@ -506,6 +619,21 @@ class World {
     }
   }
 
+  objectFromScreen(x, y) {
+    var pos = this.camera.fromScreen(x, y);
+    var scripts = this.scripts;
+    var x = pos.x | 0;
+    var y = -pos.y | 0;
+    for (var i=scripts.length; i--;) {
+      var script = scripts[i];
+      var o = script.objectFromPoint(x - script.x, y - script.y);
+      if (o) console.log(o);
+      if (o) return o;
+    }
+    //return this;
+  }
+  
+
   touchDown(e) {
     e.preventDefault();
     lastTouch = new Vec(e.touches[0].pageX, e.touches[0].pageY);
@@ -530,9 +658,147 @@ class World {
     this.lastDelta = undefined;
   }
 
-  // TODO Safari 9.1 gestureDown/Change/Up to zoom
-  // TODO click and drag background to party
+  mouseDown(e) {
+    var p = {clientX: e.clientX, clientY: e.clientY, identifier: this};
+    if (!this.startFinger(p, e)) return;
+    this.fingerDown(p, e);
+  }
+  mouseMove(e) {
+    var p = {clientX: e.clientX, clientY: e.clientY, identifier: this};
+    // this.updateMouse(p, e);
+    this.fingerMove(p, e);
+  }
+  mouseUp(e) {
+    var p = {clientX: e.clientX, clientY: e.clientY, identifier: this};
+    // this.updateMouse(p, e);
+    this.fingerUp(p, e);
+  }
+
+  startFinger(p, e) {
+    /*
+    this.updateMouse(p, e);
+    this.menuMouseDown(e);
+
+    var pressType = this.pressType(e);
+    if (pressType !== 'workspace' && (pressType !== 'input' || e.button === 2)) return false;
+    */
+    if (this.dragging) {
+      this.drop();
+      return false;
+    }
+    return true;
+  }
+
+  createFinger(id) {
+    if (id === this) {
+      var g = this;
+    } else {
+      this.destroyFinger(id);
+      g = this.getFinger(id);
+    }
+    return g;
+  }
+
+  getFinger(id) {
+    if (id === this) return this;
+    var g = this.fingers[id];
+    if (g) return g;
+    return this.fingers[id] = {feedback: this.createFeedback()};
+  }
+
+  destroyFinger(id) {
+    var g = id === this ? this : this.fingers[id];
+    if (g) {
+      if (g.dragging) this.drop(g);
+
+      g.pressed = false;
+      g.pressObject = null;
+      g.dragging = false;
+      g.resizing = false;
+      g.shouldDrag = false;
+      g.shouldResize = false;
+      g.dragScript = null;
+
+      delete this.fingers[id];
+    }
+  }
+
+  fingerDown(p, e) {
+    // TODO click and drag background to scroll
+
+    var g = this.createFinger(p.identifier);
+    g.pressX = g.mouseX = p.clientX;
+    g.pressY = g.mouseY = p.clientY;
+    g.pressObject = this.objectFromScreen(g.pressX, g.pressY);
+    g.shouldDrag = false;
+    // g.shouldResize = false;
+    
+    if (g.pressObject) {
+      var leftClick = e.button === 0 || e.button === undefined;
+      if (e.button === 2 || leftClick && e.ctrlKey) {
+        // TODO menus
+      } else if (leftClick) {
+        // if (g.pressObject.isResizable) {
+        //   var pos = g.pressObject.worldPosition;
+        //   g.shouldResize = g.pressObject.resizableAt(g.pressX - pos.x, g.pressY - pos.y);
+        // }
+        g.shouldDrag = true; //!g.shouldResize && g.pressObject.isDraggable && !((g.pressObject.isTextArg) && e.target === g.pressObject.field); // TODO
+      }
+    }
+    if (g.shouldDrag) {
+      document.activeElement.blur();
+      e.preventDefault();
+    }
+
+    g.pressed = true;
+    g.dragging = false;
+  }
+
+  fingerMove(p, e) {
+    var g = this.getFinger(p.identifier);
+    g.mouseX = p.clientX;
+    g.mouseY = p.clientY;
+    if (g.dragging) {
+      var pos = this.camera.fromScreen(g.dragX + g.mouseX, g.dragY + g.mouseY);
+      g.dragScript.moveTo(pos.x | 0, -pos.y | 0);
+      // this.showFeedback(g); // TODO
+      e.preventDefault();
+
+    } else if (g.pressed && g.shouldDrag) {
+      var block = g.pressObject.dragObject;
+      // g.dragWorkspace = block.workspace;
+      // g.dragPos = block.workspacePosition;
+      // g.dragState = block.state;
+      var pos = block.worldPosition;
+      this.grab(block.detach(), pos.x - g.pressX, pos.y - g.pressY, g);
+      e.preventDefault();
+
+    //} else if (g.resizing) {
+    //  g.pressObject.resizeTo(Math.max(g.pressObject.minWidth, (g.dragWidth + g.mouseX) / this._blockScale | 0), Math.max(g.pressObject.minHeight, (g.dragHeight + g.mouseY) / this._blockScale | 0));
+
+    //} else if (g.shouldResize) {
+    //  g.resizing = true;
+    //  g.dragWidth = g.pressObject.width * this._blockScale - g.pressX;
+    //  g.dragHeight = g.pressObject.height * this._blockScale - g.pressY;
+    }
+  }
+
+  fingerUp(p, e) {
+    var g = this.getFinger(p.identifier);
+    g.mouseX = p.clientX;
+    g.mouseY = p.clientY;
+    if (g.dragging) {
+      this.drop(g);
+    } else if (g.resizing) {
+    } else if (g.shouldDrag || g.shouldResize) {
+      g.pressObject.click(g.pressX, g.pressY);
+    }
+    this.destroyFinger(p.identifier);
+  }
+
+  // TODO Safari 9.1 has *actual* gesture events: gestureDown/Change/Up to zoom
 }
+World.prototype.isWorkspace = true;
 
 document.body.appendChild(new World().el);
 
